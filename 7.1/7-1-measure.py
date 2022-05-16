861e8aa41f1737d610c4fc4c4dfa9d4076257247
Ponomarev <ponomarev.bs@phystech.edu>
2022-05-16 09:21:28 +0300

    YEEEEEAAAH

--- 7.1/7-1-measure.py
+++ 7.1/7-1-measure.py
@@ -1,74 +1,94 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpio
 import time
+import matplotlib.pyplot as mplot
 
-dac = [26, 19, 13, 6, 5, 11, 9, 10]
+
+dac = [ 26, 19, 13, 6, 5, 11, 9, 10 ]
+leds = [ 21, 20, 16, 12, 7, 8, 25, 24 ]
 comp = 4
-bits = len(dac)
-levels = 2**bits
-maxVoltage = 3.3
 troyka = 17
-leds = [21, 20, 16, 12, 7, 8, 25, 24]
-values_voltage = []
+Vref = 3.3
+bdepth = 8 #хардкод разрядности
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(dac, GPIO.OUT)
-GPIO.setup(comp, GPIO.IN)
-GPIO.setup(leds, GPIO.OUT)
-GPIO.setup(troyka, GPIO.OUT)
 
-def bin_2(value):
-    return [int(i) for i in bin(value)[2:].zfill(8)]
+gpio.setmode( gpio.BCM )
 
-def num2dac(value):
-    signal = bin_2(value)
-    GPIO.output(dac, signal)
-    return signal
+gpio.setup( dac, gpio.OUT, initial=gpio.LOW )
+gpio.setup( leds, gpio.OUT, initial=gpio.LOW )
+gpio.setup( troyka, gpio.OUT, initial=gpio.HIGH )
+gpio.setup( comp, gpio.IN )
+
+
+def decimal2binary( value ):
+    return [int(bit) for bit in bin( value ) [2:].zfill( bdepth )]
 
 def adc():
-    left = 0
-    right = 256
-    value = 128
-    
-    while right - value != 1:
-        signal = num2dac(value)
-        time.sleep(0.01)
-        compValue = GPIO.input(comp)
-        if compValue:
-            left = value
-            value += (right - value) // 2
-        else:
-            right = value
-            value -= (value - left) // 2
-    GPIO.output(leds, signal)
-    voltage = value / levels * maxVoltage
-    return voltage                                                                    
+    retvalue = 0
+    decVtest = 0
+    for testbit in reversed( range( bdepth ) ):
+        decVtest = retvalue + 2 ** testbit
+        gpio.output( dac, decimal2binary( decVtest ) )
+        time.sleep( 0.001 )
+        compsignal = gpio.input( comp )
+
+        if compsignal == 1: 
+            retvalue += 2 ** testbit
+
+    return retvalue
+
 
 try:
-    
-    start_Time = time.time()
-    GPIO.output(troyka, 0)
+    data = []
+    start_time = time.time()
+    phase1_passed = False
 
-    voltage = adc()
-    values_voltage.append(voltage)
-    while voltage < 1.6:
-        print("raise", voltage)
-        voltage = adc()
-        values_voltage.append(voltage)
 
-    GPIO.output(troyka, 1)
+    while True:
+        decVfind = adc()
+        binVfind = decimal2binary( decVfind )
 
-    while voltage > 0.61:
-        print("decrise", voltage)
-        voltage = adc()
-        values_voltage.append(voltage)
-        
-    stop_Time = time.time()
+        volume = round( decVfind / ( 2 ** bdepth ) * bdepth ) 
+        gpio.output( leds[:volume], 1 )
+        gpio.output( leds[volume:], 0 )
 
-    with open('data.txt', 'w') as f:
-        f.write('\n'.join([str(val) for val in values_voltage]))
-    
+        print('decimal V = {}'.format( decVfind ))
+        print('binary V = {}'.format( binVfind ))
+        print('V = {}'.format( decVfind / ( 2 ** bdepth ) * Vref ))
+        print( '' )
+
+
+        data = data + [ decVfind ]
+
+        if decVfind >= 0.97 * ( 2 ** bdepth ):
+            gpio.output( troyka, 0 )
+            phase1_passed = True
+
+        if phase1_passed == True | decVfind <= 0.02 * ( 2 ** bdepth ):
+            break
+
+    end_time = time.time()
+    duration = end_time - start_time
+
+    print('duration = {} s'.format( duration ))
+
+    mplot.plot( data )
+
+    data_str = "\n".join( [ str( item ) for item in data ] )
+    with open( "data.txt", "w" ) as datafile:
+        datafile.write( data_str )
+
+    sample_rate = len( data ) / duration
+    quantization = Vref / ( 2 ** bdepth )
+
+    print('sample rate = {} Hz'.format( sample_rate ))
+    print('quantization = {} V'.format( quantization ))
+
+    with open( "settings.txt", "w" ) as settingsfile:
+        settingsfile.write( 'samplerate={}\n'.format( sample_rate ) )
+        settingsfile.write( 'quantization={}'.format( quantization ) )
+
+    mplot.show()
+
 finally:
-    time.sleep(2)
-    GPIO.output(dac, 0)
-    GPIO.cleanup()
-
+    gpio.output( dac + leds + [troyka], 0 )
+    gpio.cleanup()
